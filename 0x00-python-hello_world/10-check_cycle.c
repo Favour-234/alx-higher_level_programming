92f2313a7decdf71fa7985517395dc3c7dc3aab2
Favour-234 <agboolafavour342@gmail.com>
2023-11-30 02:29:01 -0800

    cal

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * check_cycle - to check list cycle
+ * @list: linked list
+ * Return: 0 if not true or 1 if true
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *fast, *slow;
+
+	if (!list || !list->next)
+		return (0);
+	fast = list;
+	slow = list;
+
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
